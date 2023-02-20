521909e833608c144866e27f865eb30568a0c1f0
apella1 <johncollons5@gmail.com>
2023-02-20 22:49:44 +0300

    Add project README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lyrics Search
+
+## About The Project
+
+This project is a simple lyrics search app. It uses the [Genius API](https://docs.genius.com/) to search for lyrics.
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps:
+
+- Clone the repo to your development environment
+- Switch to the project's directory.
+- Run the development server - Visual Studio Code's Live Server extension is a popular choice.
--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,73 +1,75 @@
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const result = document.getElementById('result');
-const more = document.getElementById('more');
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
 
-const apiURL = 'https://api.lyrics.ovh';
+const apiURL = "https://api.lyrics.ovh";
 
-// searching by artist or song 
+// searching by artist or song
 
-async function searchSongs(term) { 
-    const res = await fetch(`${apiURL}/suggest/${term}`);
-    const data = await res.json();
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
 
-    showData(data);
+  showData(data);
 }
 
-// displaying the song and artist in the DOM 
+// displaying the song and artist in the DOM
 
-function showData (data) { 
-    result.innerHTML = `
-        <ul class='songs'>${data.data.map(song => `<li>
+function showData(data) {
+  result.innerHTML = `
+        <ul class='songs'>${data.data
+          .map(
+            (song) => `<li>
         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
         <button class='btn' data-artist='${song.artist.name}' data.songtitle='${song.title}'>Get Lyrics</button> </li>`
-    ).join('')}
+          )
+          .join("")}
     </ul>
     `;
 
-    if (data.prev || data.next) { 
-        more.innerHTML = `
+  if (data.prev || data.next) {
+    more.innerHTML = `
             ${
-                data.prev
-                    ? `<button class='btn' onClick='getMoreSongs('${data.prev}')>Prev</button>'`
-                    : ''
+              data.prev
+                ? `<button class='btn' onClick='getMoreSongs('${data.prev}')>Prev</button>'`
+                : ""
             }
 
             ${
-                data.next
-                    ? `<button class='btn' onClick='getMoreSongs('${data.next}')>Next</button>'`
-                    : ''
+              data.next
+                ? `<button class='btn' onClick='getMoreSongs('${data.next}')>Next</button>'`
+                : ""
             }
         `;
-    } else { 
-        more.innerHTML = '';
-    }
+  } else {
+    more.innerHTML = "";
+  }
 }
 
-// getting the previous and next songs 
+// getting the previous and next songs
 
-async function getMoreSongs(url) { 
-    const res = await fetch('https://cors-anywhere.herokuapp.com/${url}');
-    const data = await res.json();
+async function getMoreSongs(url) {
+  const res = await fetch("https://cors-anywhere.herokuapp.com/${url}");
+  const data = await res.json();
 
-    showData();
+  showData();
 }
 
-// getting song lyrics 
+// getting song lyrics
 
-async function getLyrics (artist, songTitle) { 
-    const res = await fetch('${apiURL}/v1/${artist}/${songTitle}');
-    const data = await res.json();
+async function getLyrics(artist, songTitle) {
+  const res = await fetch("${apiURL}/v1/${artist}/${songTitle}");
+  const data = await res.json();
 
+  if (data.error) {
+    result.innerHTML = data.error;
+  } else {
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+    <span>${lyrics}</span>;
+  }
 
-    if (data.error) { 
-        result.innerHTML = data.error;
-    } else { 
-        const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
-        <span>${lyrics}</span>;
-    }
-
-    more.innerHTML = '';
+  more.innerHTML = "";
 }
 
-// the event listeners 
+// the event listeners
